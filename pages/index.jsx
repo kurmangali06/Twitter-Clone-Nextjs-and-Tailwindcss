f1059eefda1954251c0a43dda0b406e086ab3770
Курмангали <kurmangali0606@gmail.com>
2022-09-27 18:00:16 +0600

    craete Feed componets and its header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
       <main className='flex min-h-screen max-w-7xl mx-auto '>
       <Sidebar/>
+      <Feed/>
       </main>
     </div>
   )
